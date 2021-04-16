60e787dd844e74a59bd52f10b76575e8cf8dba0b
Irfan Ali <Irfan.Ali@mLogica.com>
2021-04-16 10:57:00 +0500

    Add project files.

---
+++ JSONPatch/Controllers/WeatherForecastController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JSONPatch.Models;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+//https://www.c-sharpcorner.com/article/json-patch-in-asp-net-core-web-api/
+namespace JSONPatch.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+            [HttpGet]
+            public IActionResult JsonPatchWithModelState()
+            {
+                var customer = CreateCustomer();
+                return new ObjectResult(customer);
+            }
+
+            [HttpPatch]
+            public IActionResult JsonPatchWithModelState(
+                [FromBody] JsonPatchDocument<Customer> patchDoc)
+            {
+                if (patchDoc != null)
+                {
+                    var customer = CreateCustomer();
+
+                    patchDoc.ApplyTo(customer, (Microsoft.AspNetCore.JsonPatch.Adapters.IObjectAdapter)ModelState);
+
+                    if (!ModelState.IsValid)
+                    {
+                        return BadRequest(ModelState);
+                    }
+
+                    return new ObjectResult(customer);
+                }
+                else
+                {
+                    return BadRequest(ModelState);
+                }
+            }
+
+            private Customer CreateCustomer()
+            {
+                return new Customer
+                {
+                    CustomerName = "John",
+                    Orders = new List<Order>()
+                {
+                    new Order
+                    {
+                        OrderName = "Order0",
+                        OrderType="Urgent"
+                    },
+                    new Order
+                    {
+                        OrderName = "Order1",
+                        OrderType="Normal"
+                    }
+                }
+                };
+            }
+        }
+}
---
+++ JSONPatch/Models/VideoGame.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace JsonPatch.Models
+{
+    // VideoGame.cs  
+    public partial class VideoGame
+    {
+        [Key]
+        public virtual int Id { get; set; }
+
+        public virtual string Title { get; set; }
+
+        public virtual string Publisher { get; set; }
+
+        public virtual DateTime? ReleaseDate { get; set; }
+
+        public VideoGame(int id, string title, string publisher, DateTime? releaseDate)
+        {
+            Id = id;
+            Title = title;
+            Publisher = publisher;
+            ReleaseDate = releaseDate;
+        }
+    }
+}
